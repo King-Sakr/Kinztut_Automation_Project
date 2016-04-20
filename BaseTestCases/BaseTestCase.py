2605f40839024ed737a9fd2f19a7f84cae20e4ea
msakr <m.sakr@live.co.uk>
2016-04-20 07:30:02 +0200

    update

--- BaseTestCases/BaseTestCase.py
+++ BaseTestCases/BaseTestCase.py
@@ -18,6 +18,22 @@ class BaseTestCase(unittest.TestCase):
         self.driver.close()
 
 
+class BaseTestCase_Login(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        fp = webdriver.FirefoxProfile('C:/Profiles')
+        cls.driver = webdriver.Firefox(fp)
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
+        cls.driver.get("http://www.kinztut.com")
+        cls.driver.maximize_window()
+        HomePage.Login_Homepage(cls)
+
+    @classmethod
+    def TearDown(self):
+        HomePage.Logout_Homepage(cls)
+        self.driver.close()
+
 
 if __name__ == '__main__':
     unittest.main()
